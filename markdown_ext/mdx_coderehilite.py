0450da2ce5d6668563b3f05cd916fc20c0a03df1
renn999 <renn999@gmail.com>
2012-10-29 17:08:31 +0800

    cmd premote & markdown ext re fix

--- markdown_ext/mdx_coderehilite.py
+++ markdown_ext/mdx_coderehilite.py
@@ -5,7 +5,7 @@ import markdown
 
 # Global vars
 Octo_BLOCK_RE = re.compile( \
-    r'^<table class="codehilitetable"><tr><td class="linenos"><div class="linenodiv"><pre>(?P<line_num>.*)</pre></div></td><td class="code"><div class="codehilite"><pre>(?P<code>.*)</pre></div>\n?</td></tr></table>',
+    r'^<table class="codehilitetable"><tr><td class="linenos"><div class="linenodiv"><pre>(?P<line_num>.*?)</pre></div></td><td class="code"><div class="codehilite"><pre>(?P<code>.*?)</pre></div>\n?</td></tr></table>',
     re.MULTILINE|re.DOTALL
     )
 
@@ -23,28 +23,21 @@ class coderehiliteExtension(markdown.Extension):
 class coderehiliteBlockPreprocessor(markdown.postprocessors.Postprocessor):
 
     def run(self, text):
-        box = []
-        j=1
-        s_text = text.split('</td></tr></table>')
-        for i in s_text[:-1]:
-            i = i+'</td></tr></table>'
-            while 1:
-                m = Octo_BLOCK_RE.search(i)
-                if m:
-                    num_ln = []
-                    for j in m.group('line_num').split('\n'):
-                        j = '<span class=\'line-number\'>'+ j + '</span>'
-                        num_ln += [j]
-                    code_ln=[]
-                    for j in m.group('code').split('\n'):
-                        j = "<span class='line'>" + j +'</span>'
-                        code_ln += [j]
-                    placeholder = '<figure class="code"><figcaption><span></span></figcaption><div class="highlight"><table><tr><td class="gutter"><pre class="line-numbers">'+ u'\n'.join(num_ln) + '</pre></td><td class=\'code\'><pre><code>'+u'\n'.join(code_ln)+'</code></pre></td></tr></table></div></figure>'
-                    i = '%s\n%s\n%s'% (i[:m.start()], placeholder, i[m.end():])
-                else:
-                    break
-            box = box + [i]
-        text = '\n'.join(box) + s_text[-1]
+        while 1:
+            m = Octo_BLOCK_RE.search(text)
+            if m:
+                num_ln = []
+                for j in m.group('line_num').split('\n'):
+                    j = '<span class=\'line-number\'>'+ j + '</span>'
+                    num_ln += [j]
+                code_ln=[]
+                for j in m.group('code').split('\n'):
+                    j = "<span class='line'>" + j +'</span>'
+                    code_ln += [j]
+                placeholder = '<figure class="code"><figcaption><span></span></figcaption><div class="highlight"><table><tr><td class="gutter"><pre class="line-numbers">'+ u'\n'.join(num_ln) + '</pre></td><td class=\'code\'><pre><code>'+u'\n'.join(code_ln)+'</code></pre></td></tr></table></div></figure>'
+                text = '%s\n%s\n%s'% (text[:m.start()], placeholder, text[m.end():])
+            else:
+                break
         return text
 
 
